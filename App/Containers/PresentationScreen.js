587c35324a2797d3477be023245447aca7bc2c04
Randy Daal <randy@randynamic.com>
2016-12-30 14:39:30 +0100

    Did some small cleanup stuff

--- App/Containers/PresentationScreen.js
+++ App/Containers/PresentationScreen.js
@@ -1,7 +1,7 @@
 // @flow
 
 import React from 'react'
-import { ScrollView, Text, Image, View } from 'react-native'
+import { ScrollView, Image, View } from 'react-native'
 import { Images } from '../Themes'
 import RoundedButton from '../Components/RoundedButton'
 import { Actions as NavigationActions } from 'react-native-router-flux'
@@ -13,42 +13,18 @@ export default class PresentationScreen extends React.Component {
   render () {
     return (
       <View style={styles.mainContainer}>
-        <Image source={Images.background} style={styles.backgroundImage} resizeMode='stretch' />
         <ScrollView style={styles.container}>
           <View style={styles.centered}>
-            <Image source={Images.clearLogo} style={styles.logo} />
+            <Image source={Images.slackify} style={styles.logo} />
           </View>
 
-          <View style={styles.section} >
-            <Text style={styles.sectionText} >
-              Default screens for development, debugging, and alpha testing
-              are available below.
-            </Text>
-          </View>
-
-          <RoundedButton onPress={NavigationActions.componentExamples}>
-            Component Examples Screen
-          </RoundedButton>
-
-          <RoundedButton onPress={NavigationActions.usageExamples}>
-            Usage Examples Screen
-          </RoundedButton>
-
-          <RoundedButton onPress={NavigationActions.apiTesting}>
-            API Testing Screen
-          </RoundedButton>
-
-          <RoundedButton onPress={NavigationActions.theme}>
-            Theme Screen
+          <RoundedButton onPress={NavigationActions.login}>
+            Login
           </RoundedButton>
 
           <RoundedButton onPress={NavigationActions.deviceInfo}>
             Device Info Screen
           </RoundedButton>
-
-          <View style={styles.centered}>
-            <Text style={styles.subtitle}>Made with ❤️ by Infinite Red</Text>
-          </View>
 
         </ScrollView>
       </View>
